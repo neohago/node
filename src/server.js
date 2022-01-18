a99aa4333babfbdf7932b22d8a152a80af080a4c
chanseob jeon <jeon@twosun.com>
2022-01-18 18:47:52 +0900

    second

--- src/server.js
+++ src/server.js
@@ -1,1 +1,24 @@
-console.log("hello");
+import express from "express";
+import http from "http";
+import WebSocket from "ws";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (_, res) => res.redirect("/"));
+
+const handelListen = () => console.log(`Listening on http://localhost:3000`);
+
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+function handleConnection(socket) {
+    console.log(socket)
+}
+wss.on("connection", handleConnection);
+
+server.listen(3000, handelListen);
